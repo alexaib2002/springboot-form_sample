28e7031e57126131e690ec460e5dd1277caed991
alexaib2002 <alexaib@outlook.es>
2023-11-06 12:37:00 +0100

    Code simplification, Thymeleaf error handling

--- src/main/java/org/alexaib/springlearn/springbootform/app/controllers/FormController.java
+++ src/main/java/org/alexaib/springlearn/springbootform/app/controllers/FormController.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.stream.Collectors;
 
 @Controller
 public class FormController {
@@ -37,10 +34,6 @@ public class FormController {
     @PostMapping("/form")
     public String processForm(@Valid User user, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            model.addAttribute("error", result.getFieldErrors()
-                    .stream().collect(Collectors.toMap(FieldError::getField, f ->
-                        String.format("Field %s %s", f.getField(), f.getDefaultMessage())
-                    )));
             return "form";
         }
         model.addAttribute("user", user);
