9f45f421fbb814dee846edcf390c814eacb77016
alexaib2002 <alexaib@outlook.es>
2023-11-06 10:54:08 +0100

    Error population, user autowiring

--- src/main/java/org/alexaib/springlearn/springbootform/app/controllers/FormController.java
+++ src/main/java/org/alexaib/springlearn/springbootform/app/controllers/FormController.java
@@ -1,21 +1,48 @@
 package org.alexaib.springlearn.springbootform.app.controllers;
 
+import jakarta.validation.Valid;
 import org.alexaib.springlearn.springbootform.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class FormController {
 
+    private User user;
+
+    @Autowired
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("user", user);
         return "form";
     }
 
+    /**
+     * @param user   Validated java object
+     * @param result Must be after the validated object
+     * @param model  MVC view
+     * @return Result webpage
+     */
     @PostMapping("/form")
-    public String processForm(User user, Model model) {
+    public String processForm(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("error", result.getFieldErrors()
+                    .stream().collect(Collectors.toMap(FieldError::getField, f ->
+                        String.format("Field %s %s", f.getField(), f.getDefaultMessage())
+                    )));
+            return "form";
+        }
         model.addAttribute("user", user);
         return "result";
     }
--- src/main/java/org/alexaib/springlearn/springbootform/app/model/User.java
+++ src/main/java/org/alexaib/springlearn/springbootform/app/model/User.java
@@ -1,8 +1,19 @@
 package org.alexaib.springlearn.springbootform.app.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public record User(
+        @Value("")
+        @NotEmpty
         String username,
+        @Value("")
+        @NotEmpty
         String password,
+        @Value("")
+        @NotEmpty
         String email
 ) {
 }
